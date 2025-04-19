c8d0b9aff5cf481f32e39572fef88d71df2b4fec
al-jaber-nishad <mailnishad02@gmail.com>
2025-04-20 01:50:03 +0600

    feat: add name to destination_check URL

--- travel/urls/travel_recommendation_urls.py
+++ travel/urls/travel_recommendation_urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from travel.views import travel_recommendation_views as views
 
 urlpatterns = [
-    path("destination_check/", views.destination_check),
+    path("destination_check/", views.destination_check, name="destination_check"),
 ]
